a629f38ed0372b5886fa1510acbaae3e21ec51c2
wsafight <984292420@qq.com>
2023-07-28 03:08:44 +0800

    #FEAT: add icp

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -34,7 +34,8 @@ export const zh: SidebarConfig = {
         '/performance/why-b-plus-tree.md',
         '/performance/soar.md',
         '/performance/low-dimension-index.md',
-        '/performance/hot-update.md'
+        '/performance/hot-update.md',
+        '/performance/icp.md',
       ],
     },
   ],
